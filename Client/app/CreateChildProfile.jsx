4782f0679d44a8714b858c1b11ce62104c91d271
hadarkok <96580869+hadarkok@users.noreply.github.com>
2024-12-29 17:31:24 +0200

    seperated client and server into 2 reposetories

--- Client/app/CreateChildProfile.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import EditChildProfile from '../components/EditChildProfile';
-import { useRouter, useLocalSearchParams } from "expo-router";
-
-export default function CreateChildProfileScreen({ route }) {
-  return <EditChildProfile/>;
-}
