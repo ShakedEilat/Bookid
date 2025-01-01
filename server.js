c51d6b627358e09901e8a7708c62c270c1ec6825
ShakedEilat <eilat.shaked@gmail.com>
2025-01-01 14:33:12 +0200

    modified server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { json } = require('body-parser');
+const  config  = require('./src/config/config');
+const { ROUTES, RESPONSE_MESSAGES } = require('./utils/consts');
+const cors = require('cors')
+
+// Import Routes
+const authRoutes = require('./src/routes/authRoutes');
+const childProfileRoutes = require('./src/routes/childProfileRoutes');
+const bookRoutes = require('./src/routes/bookRoutes');
+
+const app = express();
+
+app.use(cors())
+// Middleware
+app.use(json());
+
+// Routes
+app.use(ROUTES.AUTH, authRoutes);
+app.use(ROUTES.CHILD_PROFILES, childProfileRoutes);
+app.use(ROUTES.BOOKS, bookRoutes);
+
+// Database Connection
+mongoose
+    .connect(config.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log(RESPONSE_MESSAGES.SUCCESS))
+    .catch((err) => console.error(err));
+
+// Start Server
+app.listen(config.PORT, () => console.log(`Server running on port ${config.PORT}`));
